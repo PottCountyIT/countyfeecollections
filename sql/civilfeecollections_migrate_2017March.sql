0002cc7198c32f98b58d395649c9d7658656aa37
Michael Rains <iowamiker@hotmail.com>
2017-04-02 23:23:55 -0500

    but allow the Alter Table script in order to update the attorney existing database

--- sql/civilfeecollections_migrate_2017March.sql
+++ sql/civilfeecollections_migrate_2017March.sql
@@ -1,14 +1,14 @@
 USE [civilfeecollections]
 GO
 
---these columns are add in civilfeecollecitons_sample.sql
---ALTER TABLE dbo.Defendant
---ADD daysinjail int NULL,
---bookingnumber varchar(20) NULL,
---judgmentdate datetime NULL,
---hasjudgmentfiled bit NULL,
---judgmentfileddate datetime NULL,
---inbankruptcy bit null,
---bankruptcydatefiled datetime NULL,
---bankruptcyenddate datetime NULL;
---GO
+--these columns are added in civilfeecollecitons_sample.sql for new databases
+ALTER TABLE dbo.Defendant
+ADD daysinjail int NULL,
+bookingnumber varchar(20) NULL,
+judgmentdate datetime NULL,
+hasjudgmentfiled bit NULL,
+judgmentfileddate datetime NULL,
+inbankruptcy bit null,
+bankruptcydatefiled datetime NULL,
+bankruptcyenddate datetime NULL;
+GO
