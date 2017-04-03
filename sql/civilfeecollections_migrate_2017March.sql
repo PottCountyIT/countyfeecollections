604e2c208a00a956c599138c3afab8632c967713
Michael Rains <iowamiker@hotmail.com>
2017-04-02 23:22:29 -0500

    add all columns to create table script, don't need alter table script to add the 8 new columns now

--- sql/civilfeecollections_migrate_2017March.sql
+++ sql/civilfeecollections_migrate_2017March.sql
@@ -1,13 +1,14 @@
 USE [civilfeecollections]
 GO
 
-ALTER TABLE dbo.Defendant
-ADD daysinjail int NULL,
-bookingnumber varchar(20) NULL,
-judgmentdate datetime NULL,
-hasjudgmentfiled bit NULL,
-judgmentfileddate datetime NULL,
-inbankruptcy bit null,
-bankruptcydatefiled datetime NULL,
-bankruptcyenddate datetime NULL;
-GO
+--these columns are add in civilfeecollecitons_sample.sql
+--ALTER TABLE dbo.Defendant
+--ADD daysinjail int NULL,
+--bookingnumber varchar(20) NULL,
+--judgmentdate datetime NULL,
+--hasjudgmentfiled bit NULL,
+--judgmentfileddate datetime NULL,
+--inbankruptcy bit null,
+--bankruptcydatefiled datetime NULL,
+--bankruptcyenddate datetime NULL;
+--GO
